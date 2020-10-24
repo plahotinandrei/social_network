4ec6c8fe9911f88c1289856a3d1611356182f049
plahotinandrei <40336565+plahotinandrei@users.noreply.github.com>
2020-10-24 09:16:43 +0300

    Add files via upload

---
+++ src/StoreContext.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const StoreContext = React.createContext(null);
+
+const Provider = (props) => {
+    return (
+        <StoreContext.Provider value={props.store}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+
+export default StoreContext;
+export {Provider};
