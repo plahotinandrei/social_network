8511faae24e44684aa8ecd92c695fe99fad97691
plahotinandrei <40336565+plahotinandrei@users.noreply.github.com>
2020-10-24 09:10:37 +0300

    add project

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+let path = require('path');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let conf = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist/'),
+        filename: 'main.js',
+        publicPath: 'dist/'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [
+                            "@babel/plugin-transform-react-jsx",
+                            ["@babel/plugin-proposal-decorators", { "legacy": true }],
+                            ["@babel/plugin-proposal-class-properties", { "loose" : true }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.module\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development'
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                localIdentName: '[local]__[sha1:hash:hex:7]'
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /^((?!\.module).)*css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development',
+                        }
+                    }, 
+                    'css-loader'
+                ]
+            },
+            { test: /\.(png|jpe?g|svg|gif)$/i, loader: 'url-loader?limit=8192' }
+        ]
+    }, 
+    devServer: {
+        historyApiFallback: true,
+        overlay: true,  
+        port: 3000
+    }
+};
+
+module.exports = (env, options) => {
+    conf.devtool = options.mode === "production" ? 
+                    false :
+                    "cheap-module-eval-source-map";
+    return conf;
+};
